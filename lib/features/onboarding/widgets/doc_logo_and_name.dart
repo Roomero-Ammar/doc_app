7e0f75f8a9f61d0cc436930d0375d8c02d83124a
AmmarYaser <ay1513600@gmail.com>
2025-02-12 04:43:01 +0200

    Add new assets and update onboarding screen
    Added new image and SVG assets for the application, including doctor images, patterns, logos, and icons. Updated the onboarding screen to include a new widget for displaying the doctor logo and name. Also, added navigation and string extensions for better utility functions.

---
+++ lib/features/onboarding/widgets/doc_logo_and_name.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class DocLogoAndName extends StatelessWidget {
+  const DocLogoAndName({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
