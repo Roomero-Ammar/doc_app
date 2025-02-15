962a57ed4e0ce1493a1a616715e9372d9f034990
AmmarYaser <ay1513600@gmail.com>
2025-02-16 01:47:10 +0200

    feat: create login_cubit.dart, login_state.dart and login_state.freezed.dart

---
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: avoid_types_as_parameter_names
+
+import 'package:doctors_speciality/features/login/data/models/login_request_body.dart';
+import 'package:doctors_speciality/features/login/data/models/login_response.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:doctors_speciality/features/login/data/repo/login_repo.dart';
+
+import 'login_state.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+
+  LoginCubit(this._loginRepo) : super(const LoginState.initial());
+
+  void emitLoginState(LoginRequestBody loginRequestBody) async {
+    emit(LoginState.loading());
+    final response = await _loginRepo.login(loginRequestBody);
+
+    response.when(success: (loginResponse) {
+      emit(LoginState.success(loginResponse));
+    }, failure: (error) {
+      emit(LoginState.error(error: error.apiErrorModel.message ?? ''));
+    });
+  }
+}
